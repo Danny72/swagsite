a0eb206902e37f04c953f925b7d66420cd6d6959
Danny Pike <danny72@gmail.com>
2012-11-06 13:10:21 +0000

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swagsite::Application.config.secret_token = '35186243cda43a7aae994f9a6ed37532d59c67cf5acc441a06cd583bb4d198d03ff94a87ef398e464d0a8888b973738f73367fee59661879dcb73cb804295451'
